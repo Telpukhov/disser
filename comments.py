297b64011bda7a0fe55f2808da45225d06a0ffde
Telpukhov <nofrost@inbox.ru>
2018-03-03 17:08:28 +0400

    commit

--- comments.py
+++ comments.py
@@ -1,6 +1,5 @@
-from lxml import etree
-import zipfile
-from pprint import  pprint
+import json
+from pprint import pprint
 
 def tanimoto(s1, s2):
     a, b, c = len(s1), len(s2), 0.0
@@ -9,29 +8,6 @@ def tanimoto(s1, s2):
             c += 1
     return c / (a + b - c)
 
-ooXMLns = {'w':'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
-docxFileName = "main.docx"
-
-def get_comments(docxFileName):
-  docxZip = zipfile.ZipFile(docxFileName)
-  commentsXML = docxZip.read('word/comments.xml')
-  et = etree.XML(commentsXML)
-  comments = et.xpath('//w:comment',namespaces=ooXMLns)
-  FA = []
-  FAA = {}
-  ind = 0
-  for c in comments:
-    ind += 1
-    comment = (c.xpath('string(.)'))
-    arr = comment.split('$$$')
-    tmp = [i for i in range(len(FA), len(FA)+len(arr))]
-    FA += arr
-    FAA[ind] = tmp
-  return (FA, FAA)
-
-
-  pprint(FA)
-  pprint(FAA)
 
 def clean_array(FA):
     for i in range(len(FA)):
@@ -43,6 +19,8 @@ def clean_array(FA):
         print(str(i)+' - ', str(max))
 
 
+with open('comments.txt', 'r') as file:
+    FA = file.readlines()
 
-FA, FAA = get_comments(docxFileName)
-clean_array(FA)
+FAA = json.load(open('array.txt'))
+pprint(FAA)
---
+++ get_comments.py
@@ -0,0 +1,38 @@
+from lxml import etree
+import zipfile
+
+ooXMLns = {'w':'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
+docxFileName = "main.docx"
+
+def get_comments(docxFileName):
+  docxZip = zipfile.ZipFile(docxFileName)
+  commentsXML = docxZip.read('word/comments.xml')
+  et = etree.XML(commentsXML)
+  comments = et.xpath('//w:comment',namespaces=ooXMLns)
+  FA = []
+  FAA = {}
+  ind = 0
+
+  for c in comments:
+    ind += 1
+    comment = (c.xpath('string(.)'))
+    arr = comment.split('$$$')
+    tmp = [i for i in range(len(FA), len(FA)+len(arr))]
+    FA += arr
+    FAA[ind] = tmp
+
+  return (FA, FAA)
+
+
+
+
+
+FA, FAA = get_comments(docxFileName)
+
+import json
+with open('array.txt', 'w') as outfile:
+    json.dump(FAA, outfile)
+
+for comment in FA:
+    print(comment)
+# здесь надо скопипастить из консоля в comments.txt
